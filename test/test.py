33e3de231ddcb2d98b3c405e657632782d90f873
Yifan Yang <yifany@csail.mit.edu>
2020-03-02 22:06:55 -0500

    test travis job matrix

--- test/test.py
+++ test/test.py
@@ -2,5 +2,10 @@ import torch
 import numpy as np
 from PIL import Image
 from tqdm import tqdm
+import platform
 
-print('hello world!')
+a = torch.Tensor([1,2,3])
+
+print(platform.linux_distribution())
+print('hello tensor!')
+print(a)
