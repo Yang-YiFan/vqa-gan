24903fbb568c1755a8a7642cb666cc3094e79bfa
Yifan Yang <yifany@csail.mit.edu>
2020-03-02 23:05:45 -0500

    print os version

--- test/test.py
+++ test/test.py
@@ -2,10 +2,13 @@ import torch
 import numpy as np
 from PIL import Image
 from tqdm import tqdm
-import platform
+import platform, os
 
 a = torch.Tensor([1,2,3])
 
 print(platform.linux_distribution())
+print(platform.system())
+print(platform.release())
+print(os.name)
 print('hello tensor!')
 print(a)
